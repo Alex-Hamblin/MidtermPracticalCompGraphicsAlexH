bc375aa58a99044aa0d0a21270741f37cc566ae3
Alex-Hamblin <alexmachamblin@gmail.com>
2024-10-09 15:57:39 -0400

    clouds

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    bool spawncloud;
+    GameObject cloud;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,13 +13,20 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (spawncloud) 
         {
-
+            spawncloud = false;
+            StartCoroutine(Clouds());
         }
 
         
+    }
+    IEnumerator Clouds()
+    {
+        Instantiate(cloud, gameObject.transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(5);
+        spawncloud=true;
     }
 }
